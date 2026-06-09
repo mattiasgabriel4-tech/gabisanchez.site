db4db6c1c47beb2696547be3ea315200c89b799b
mattiasgabriel4-tech <mattiasgabriel4@gmail.com>
2026-06-09 20:33:09 -0300

    Update and rename js/noticias.js to novedades.js

---
+++ novedades.js
@@ -0,0 +1,10 @@
+const noticias = [
+    {
+        fecha: "9 de junio de 2026",
+        categoria: "HCD",
+        titulo: "Presentación de cinco proyectos de comunicación en el Honorable Concejo Deliberante",
+        resumen: "Durante la sesión ordinaria del 9 de junio de 2026 se presentaron cinco proyectos de comunicación vinculados a pedidos de información sobre compras directas, personal municipal, licencias, proveedores y flota municipal.",
+        imagen: "img/novedades/2026-06-09.jpg",
+        link: "novedades/2026/2026-06-09-cinco-proyectos-comunicacion.html"
+    }
+];

96c8845ca403e97251f7ff1e539df82008ad1dd5
mattiasgabriel4-tech <mattiasgabriel4@gmail.com>
2026-06-09 20:41:24 -0300

    Create novedades.js

---
+++ js/novedades.js
@@ -0,0 +1,9 @@
+window.noticias = [
+    {
+        fecha: "9 de junio de 2026",
+        categoria: "HCD",
+        titulo: "Presentación de cinco proyectos de comunicación en el Honorable Concejo Deliberante",
+        resumen: "Durante la sesión ordinaria del 9 de junio de 2026 se presentaron cinco proyectos de comunicación vinculados a pedidos de información sobre compras directas, personal municipal, licencias, proveedores y flota municipal.",
+        link: "novedades/2026/2026-06-09-cinco-proyectos-comunicacion.html"
+    }
+];

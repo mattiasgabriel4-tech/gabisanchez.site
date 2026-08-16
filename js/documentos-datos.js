201acc30ef51cefe0501c61bb12a124c59d76b1b
Matías Sánchez Alfonzo <contacto.somosmasconvos@gmail.com>
2026-08-15 21:59:48 -0300

    Sistema dinámico de Ordenanzas, Proyectos y Notas leído desde Drive, con buscador y filtro por año

---
+++ js/documentos-datos.js
@@ -0,0 +1,505 @@
+// Este archivo se regenera automáticamente con extraer_documentos.py + generar_js.py
+var DOCUMENTOS_HCD = {
+  "proyectos": [
+    {
+      "anio": "2026",
+      "titulo": "Proyecto de Comunicacion: Registro de proveedores",
+      "id": "1OUFyJbtrXlFWdoouCnpCH_W1dHqyyXaO"
+    },
+    {
+      "anio": "2026",
+      "titulo": "Proyecto de Comunicacion:  registro de compra directa del ejecutivo",
+      "id": "1bZO_KLEhk7ZGSHPi22F0xH1KJl3fdCva"
+    },
+    {
+      "anio": "2026",
+      "titulo": "Proyecto de Comunicacion: censo de personal municipal",
+      "id": "15KBJp00q3p-Z2EfFNYTeKiaQL838M4j-"
+    },
+    {
+      "anio": "2026",
+      "titulo": "Proyecto de Comunicacion: estadistica de emisiónde licencias",
+      "id": "1uC6untrppXsm91_aeuh8x0hV4iHy5Pr2"
+    },
+    {
+      "anio": "2026",
+      "titulo": "Proyectonde comunicación: solicitando informe de flota municipal",
+      "id": "1MPqUEU8w1yrC4ZAE6i0q01qxOP4V2tLk"
+    },
+    {
+      "anio": "2026",
+      "titulo": "Programa integral de modernizacióny transparencia legislativa",
+      "id": "14pw2irmyH6SyFQYoCuQ4yBumaHTxckYn"
+    },
+    {
+      "anio": "2025",
+      "titulo": "Proyecto comunicacion notificando reclamos de B° Maria Auxiliadora y B° Urunday",
+      "id": "1sZXHwxUs-nUpdiFfzI9L5Pkb4tqciuLi"
+    },
+    {
+      "anio": "2025",
+      "titulo": "solicitud de asistencia medica en Paraje Tacuaruzu",
+      "id": "14W4tUk3Tgoj3LyJW_dfjAIDoEmbh1-Td"
+    },
+    {
+      "anio": "2025",
+      "titulo": "proyecto comunicacion solicitando instalacion de ssitemas de seguridad",
+      "id": "1BISH1tOkP4p0bgl8N6IdLH8et1NwEWZz"
+    },
+    {
+      "anio": "2025",
+      "titulo": "Proyecto comunicacion solicitando mantenimiento de calle al barrio Benedit y Urunday",
+      "id": "1T_PTvsQBvp1hMr1zNRQMPywlCim_1Sc6"
+    },
+    {
+      "anio": "2025",
+      "titulo": "Proyecto comunicacion sobre cajas electricas en plaza central",
+      "id": "1TkZYU-YPDN_cRb_m0kGDTIm1VCc_OW-G"
+    },
+    {
+      "anio": "2025",
+      "titulo": "Priyecto REL para inversiones privadas",
+      "id": "1Tqze7mvqG9rwFsevWgOI1AITgaSPEKD_"
+    },
+    {
+      "anio": "2025",
+      "titulo": "Nota al ministerio de educacion",
+      "id": "1TXoFwWeb8fjHP8Wmz9QCe7Ep-qIXgVba"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Proyecto solicitando informe de ordenanza tributaria",
+      "id": "1TDfvNX41tNrnFWRO8tje5wY8KsfOZGE0"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Proyecto sobre arbolado urbano",
+      "id": "1TEEP_x7CKvhERQixXfFEHxxTWNVYGalz"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Proyecto descacharrizacion",
+      "id": "1TI1ioCOh5eVIjpT1i2feW7tPXNoihsxP"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Proyecto de resolucion para reglamento interno hcd",
+      "id": "1TIbbcWnAx7E6l-HNydhJOFqqaHClTerG"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Proyecto de resolucion para realizar seciones en Tacuaruzu",
+      "id": "1TIjvEf8rYIkQVNVDdSmgKPG39dMHzMU6"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Proyecto de reconocimiento a docenees",
+      "id": "1TN1hfugTznLLiZ0AXARS8ZFmp0igFzVT"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Proyecto de ordenanza solicitando nuevo recorrido del colectivo",
+      "id": "1TUs07AIJD6j-j8tPQh2Z0zrMC-xD6cY2"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Proyecto de comunicacion solicirando informe sobre IPS",
+      "id": "1TWs6mwLWu7y2GK-KvME5FVvEcpQK6aL8"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Proyecto de comunicacionsolicitando ampliacion de frecuencias",
+      "id": "1SzpC7F_qfYkdRwcLspzoQUxaOnjoXDHZ"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Proyecto creacion de programa de prevencion del suicidio",
+      "id": "1T11ytH504V7E7OURvc3YpwDGT028G_Yb"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Primer proyecto para realizar seciones en Tacuaruzu",
+      "id": "1T22GToeE85k91nsA5FZVbsGH9XLAVkXl"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Ordenanza solicitando adhesion del municpio a sube nacional",
+      "id": "1T6vIp2HoFDXVPzPKFmo5KppsRJ23cLfz"
+    },
+    {
+      "anio": "2024",
+      "titulo": "Comunicacion sobre conflictos salariales docentes",
+      "id": "1SxtzuXyotpIo7DkxI-xufYbocJvv9yV2"
+    },
+    {
+      "anio": "2023",
+      "titulo": "Proyecto de comunicacion colectivos",
+      "id": "1SfRwONDNBdvAGoHasKhVH2q2X55ZWECa"
+    }
+  ],
+  "ordenanzas": [
+    {
+      "anio": "2026",
+      "tipo": "Ordenanza",
+      "numero": "05/2026",
+      "titulo": "ordenanza 05/2026",
+      "id": "150VG-Sk-xYvdK5IEDa1iIMynKQR-Cesm"
+    },
+    {
+      "anio": "2026",
+      "tipo": "Ordenanza",
+      "numero": "04/2026",
+      "titulo": "ordenanza 04/2026",
+      "id": "1CPfoynxaAr9pnSJpI0L6C3xikEN7cIkS"
+    },
+    {
+      "anio": "2026",
+      "tipo": "Resolución",
+      "numero": "01/2026",
+      "titulo": "Resolución 01/2026",
+      "id": "1lgg1SOpUcYFO-NXeXOSsYPA6EDmLVf8k"
+    },
+    {
+      "anio": "2026",
+      "tipo": "Declaración",
+      "numero": "01/2026",
+      "titulo": "declaracion 01/2026",
+      "id": "1dcNp1aGYPP4IBolqWJhJixmJG3v-w90l"
+    },
+    {
+      "anio": "2026",
+      "tipo": "Ordenanza",
+      "numero": "03/2026",
+      "titulo": "Ordenanza 03/2026",
+      "id": "1wNl4HoBNDep29zSUktwSM-pDk0x5avtH"
+    },
+    {
+      "anio": "2026",
+      "tipo": "Ordenanza",
+      "numero": "02/2026",
+      "titulo": "Ordenanza 02/2026",
+      "id": "1JFWzzuOM9yUIuxbnEwGLtq1GA7WTshE5"
+    },
+    {
+      "anio": "2026",
+      "tipo": "Ordenanza",
+      "numero": "01/2026",
+      "titulo": "Ordenanza 01/2026",
+      "id": "1pmfNidjNc1YDqR3jr3MS-iwoSfE63st8"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Ordenanza",
+      "numero": "13/2025",
+      "titulo": "Ordenanza y presupuestos de gatos para el año 2026 ordenanza N° 13/2025",
+      "id": "1BHJL-_ZFMwv_QyHH58hKr2hJnE-YUBcF"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Ordenanza",
+      "numero": "12/2025",
+      "titulo": "Ordenanza 12/2025",
+      "id": "1Cmgp5p2YLe1B0r9y6Hjcbt-fNskszJAz"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Ordenanza",
+      "numero": "11/2025",
+      "titulo": "Ordenanza 11/2025",
+      "id": "19EN-CiF7zkem5wsgYb7fA7DLzYCvBqqg"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Ordenanza",
+      "numero": "10/2025",
+      "titulo": "Ordenanza 10/2025",
+      "id": "1tt07cA3fsKpYm83DC0JPxxucJ65u1Zk2"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Ordenanza",
+      "numero": "05/2025",
+      "titulo": "Ordenanza N° 05/2025",
+      "id": "1zIwa9jMAc8BBi6UqFfpv3qUd7hPfNE2E"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Resolución",
+      "numero": "01/2025",
+      "titulo": "Resolucion 01-2025 calculo de gastos",
+      "id": "1vKByVpkWJ23kNQXlHhv-wRdRxvWa1YaI"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Ordenanza",
+      "numero": "04/2025",
+      "titulo": "Ordenanzaa 04-2025 encuentro de muralismo",
+      "id": "1OD5349LWgXoADq3NxOukS00JLqhXXWvt"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Ordenanza",
+      "numero": "03/2025",
+      "titulo": "Ordenanza 03-2025 convenio con Energia de Misiones",
+      "id": "1t_-pQYJMLpo5t9hEE_PXmmHqD4fUz8v_"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Ordenanza",
+      "numero": "02/2025",
+      "titulo": "Ordenanza 02-2025 incremento salarial personal",
+      "id": "1kKH-YuVgCvjgIquKy3X98BlJ12Xmsfja"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Ordenanza",
+      "numero": "01/2025",
+      "titulo": "Ordenanza 01-2025 adhesion al programa de EQUIPAMIENTO VIAL 2025",
+      "id": "1HV7y71ugQLmTunf3xRorZYo5iaiHZUKG"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Declaración",
+      "numero": "01/2025",
+      "titulo": "Declaracion 01-2025 Feria del libro",
+      "id": "1PfFVzxqtyIq4IG2a2awZQyzVzbfDgOBE"
+    },
+    {
+      "anio": "2025",
+      "tipo": "Documento",
+      "numero": "",
+      "titulo": "Balance de gastos 2024",
+      "id": "1Gv5tqUHLhNuhePJVi4XNGRPEc1XYsH2i"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Resolución",
+      "numero": "02/2024",
+      "titulo": "Resolucion 02-2024 publicar ordenanza fiscal impositiva (2012) en boletin oficial",
+      "id": "1W6RkcCxYcxJ1Pv4rLbyX96GrTrbdVOnN"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Resolución",
+      "numero": "01/2024",
+      "titulo": "Resolucion 01-2024 balance de gastos",
+      "id": "10B66kEFPp1UULSJieclQxjrhojskqLfu"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "17/2024",
+      "titulo": "Ordenanza 17-2024 contribucion solidaria para personal",
+      "id": "1moxqOW9x4YTNCylYNEBxpTUxWfTmOI1M"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "16/2024",
+      "titulo": "Ordenanza 16-2024 aumento impositivo",
+      "id": "17y5vsS7ndNZC--h3QTuR_e0bsQvHzR65"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "15/2024",
+      "titulo": "Ordenanza 15-2024 compra de caja navideñas para personal",
+      "id": "1qN-8JhhkFHFoowwTHXXLvTXCaTawAkVx"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "14/2024",
+      "titulo": "Ordenanza 14-2024 incremento de calculo de gastos",
+      "id": "1sdzRMy9h-ieL6XnOB2NKVINnVyzCPo75"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "13/2024",
+      "titulo": "Ordenanza 13-2024 presupuesto 2025",
+      "id": "11cSIScy8oOeYUME10DxJsZNTfc2yMxja"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "12/2024",
+      "titulo": "Ordenanza 12-2024",
+      "id": "1niFtKNytwV_dTdOu__UKB2ATKuQOmev6"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "11/2024",
+      "titulo": "Ordenanza 11-2024 aumento para personal",
+      "id": "1DriVYfXWB_fahIFr3Ic_T-cjhYv6kHxt"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "10/2024",
+      "titulo": "Ordenanza 10-2024 vebta de retro excavadora",
+      "id": "1I9-G9Dr7a4Wy8XGbEshuX3QVqDf2LvlF"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "09/2024",
+      "titulo": "Ordenanza 09-2024 Adhesión a red provincial de mediacion",
+      "id": "19BJsPbKFEjCxy2Ej4tlvE8nVjOcRLcBS"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "08/2024",
+      "titulo": "Ordenanza 08-2024 modificacion ordenanza 07-2011",
+      "id": "1NSn3lrGMwd5GoDn4BO5fPxGjQt46JSp9"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "07/2024",
+      "titulo": "Ordenanza 07-2024 adhesion a PROGRAMA PROVINCIAL DE EQUIPAMIENTO VIAL",
+      "id": "1h1F_GYXol5fe64CTuo7ARuZ1_0hSE2nB"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "06/2024",
+      "titulo": "Ordenanza 06-2024 incremento de calculos de gastos",
+      "id": "1BnSe4_ZHAgt5OMXFXVnh4f5xi7yXgn-a"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "05/2024",
+      "titulo": "Ordenanza 05-2024 creacion cocina cominutaria (convenio)",
+      "id": "1vdGgP3GcQv3PGEaXiZrjRQlJ5fhS5Icn"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "04/2024",
+      "titulo": "Ordenanza 04-2024 ceder cocina quincho polideportivo para cocina comunitaria",
+      "id": "1Q97YUYikKm-2KSoyED2JRUV6qO5Nrez7"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "03/2024",
+      "titulo": "Ordenanza 03-2024 modofocacion taza alumbrado publico",
+      "id": "1jukkYhqUUUJxyhKeOsKzEERSlnEksjZY"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "02/2024",
+      "titulo": "Ordenanza 02-2024 aumento salarial al personal",
+      "id": "1Nkhu_VZvSJkviw2zgXpDAupbBa7i2FDt"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Ordenanza",
+      "numero": "01/2024",
+      "titulo": "Ordenanza 01-2024 incremento salarial personal",
+      "id": "1LPDgHIDmxsG-0dXQuZOrj75bDXdBpye2"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Declaración",
+      "numero": "02/2024",
+      "titulo": "Declaracion 02-2024 Feria del Libro",
+      "id": "1gteM5Vr52htlOlaZ35Lu6jEEGpjpgdN5"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Declaración",
+      "numero": "01/2024",
+      "titulo": "Declaracion 01-2024 Interes municipal al programa PROPACYT",
+      "id": "1J1wLGxlWqhNF2YDOqV9PGaJ6ZcIIl1nj"
+    },
+    {
+      "anio": "2024",
+      "tipo": "Documento",
+      "numero": "",
+      "titulo": "Balance de gastos año 2023",
+      "id": "1Pdv45O8bhH3EHGRedmi-RNne0l7m6IV0"
+    },
+    {
+      "anio": "2023",
+      "tipo": "Ordenanza",
+      "numero": "",
+      "titulo": "Ordenanza y presupuesto 2024",
+      "id": "12meO5FQxGqCgjIdNW9XF9PbeGlcefIfh"
+    },
+    {
+      "anio": "2023",
+      "tipo": "Ordenanza",
+      "numero": "",
+      "titulo": "Ordenanza aumento para personal",
+      "id": "1rJFbEE2t_jnu7UFonlkjjbtly6cOIiGA"
+    }
+  ],
+  "notas": [
+    {
+      "anio": "2026",
+      "tipo": "recibida",
+      "titulo": "Nota recibida 02",
+      "id": "1sb2-sC-F4FScL5EnBVuSGmFNT0FbN_zb"
+    },
+    {
+      "anio": "2026",
+      "tipo": "enviada",
+      "titulo": "Nota 04",
+      "id": "1UqLzmk3DYwdKSx--rduBMd0uSZI_7aMk"
+    },
+    {
+      "anio": "2026",
+      "tipo": "recibida",
+      "titulo": "Nota recobida 01",
+      "id": "1UNhXjxEI5U9gaHrQUxBeRwAu-x4kY99p"
+    },
+    {
+      "anio": "2026",
+      "tipo": "enviada",
+      "titulo": "Nota 03",
+      "id": "1L2qGZhzupmnTtfifYyeo-H9kD2t8OVdY"
+    },
+    {
+      "anio": "2026",
+      "tipo": "enviada",
+      "titulo": "Nota 02",
+      "id": "1VBKRZbJkapFrk3zTwRXdT3JRXKqSt2hS"
+    },
+    {
+      "anio": "2026",
+      "tipo": "enviada",
+      "titulo": "Nota 01",
+      "id": "1jamEs-OgAECli-REUZRjHp7InzIiPv5H"
+    },
+    {
+      "anio": "2025",
+      "tipo": "enviada",
+      "titulo": "Nota 2",
+      "id": "1TJNTesWQJntro33yYeQraTTYLE5mYXht"
+    },
+    {
+      "anio": "2025",
+      "tipo": "enviada",
+      "titulo": "Nota 1",
+      "id": "1Ls8C1pb5Krx9s2RjeOXJe3Kr7FQoDZ2j"
+    },
+    {
+      "anio": "2024",
+      "tipo": "enviada",
+      "titulo": "Solicitud de recursos para reparacion de edificios en Tacuaruzú",
+      "id": "1HC0Yu-hj_regYU0g2PJM1HTrWn_Hca7E"
+    },
+    {
+      "anio": "2024",
+      "tipo": "enviada",
+      "titulo": "Nota para informar sobre problenatica de productores de Tacuaruzu",
+      "id": "1zviFJOvhHfMh_SDMTPbhyTID_sQSdAYo"
+    }
+  ]
+};
